3a05f65c31876642142af5b292539fccce77f7f6
Vasya Kortik <pyromatyc@gmail.com>
2018-06-15 19:47:08 +0300

    correct materials

--- D-DHelper/D-DHelper/Source/CharactersBase.cs
+++ D-DHelper/D-DHelper/Source/CharactersBase.cs
@@ -16,41 +16,6 @@ namespace D_DHelper
         private static Excel.Range CharactersBaseRange; // Cell or Cells
         // XLSX MODULE
 
-        // Characters variables
-        public static string CharactersName { get; set; }
-        public static string CharactersRace { get; set; }
-        public static string CharactersClass { get; set; }
-        public static string CharactersAlignment { get; set; }
-        public static string CharactersLevelXP { get; set; }
-        public static int CharactersStrength { get; set; }
-        public static int CharactersDexterity { get; set; }
-        public static int CharactersConstitution { get; set; }
-        public static int CharactersIntelligence { get; set; }
-        public static int CharactersWisdom { get; set; }
-        public static int CharactersCharisma { get; set; }
-        public static int CharactersArmorClass { get; set; }
-        public static int CharactersInitiative { get; set; }
-        public static int CharactersSpeed { get; set; }
-        public static char CharactersAcrobatics { get; set; }
-        public static char CharactersAnimalHandlings { get; set; }
-        public static char CharactersArcana { get; set; }
-        public static char CharactersAthletics { get; set; }
-        public static char CharactersDeception { get; set; }
-        public static char CharactersHistory { get; set; }
-        public static char CharactersInsight { get; set; }
-        public static char CharactersIntimidation { get; set; }
-        public static char CharactersInvestigation { get; set; }
-        public static char CharactersMedicine { get; set; }
-        public static char CharactersNature { get; set; }
-        public static char CharactersPerception { get; set; }
-        public static char CharactersPerformance { get; set; }
-        public static char CharactersPersuasion { get; set; }
-        public static char CharactersReligion { get; set; }
-        public static char CharactersSleightOfHand { get; set; }
-        public static char CharactersStealh { get; set; }
-        public static char CharactersSurvival { get; set; }
-        // Characters variables
-
         public static void OpenCharactersBase()
         {
         }
