c527a538c34e1380fb6b654a42580bae1dc6bca4
Vasya Kortik <pyromatyc@gmail.com>
2018-07-17 19:42:22 +0300

    80 % CharactersCreatorForm

--- D-DHelper/D-DHelper/Source/CharactersCreatorForm.Designer.cs
+++ D-DHelper/D-DHelper/Source/CharactersCreatorForm.Designer.cs
@@ -52,6 +52,22 @@
             this.INTTextbox = new System.Windows.Forms.TextBox();
             this.WISTextbox = new System.Windows.Forms.TextBox();
             this.CHATextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -134,11 +150,12 @@
             // 
             // CharactersNameTextBox
             // 
-            this.CharactersNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.CharactersNameTextBox.Location = new System.Drawing.Point(88, 114);
             this.CharactersNameTextBox.Name = "CharactersNameTextBox";
-            this.CharactersNameTextBox.Size = new System.Drawing.Size(211, 13);
+            this.CharactersNameTextBox.Size = new System.Drawing.Size(211, 20);
             this.CharactersNameTextBox.TabIndex = 7;
+            this.CharactersNameTextBox.Visible = false;
+            this.CharactersNameTextBox.MouseLeave += new System.EventHandler(this.CharactersNameTextBox_MouseLeave);
             // 
             // pictureBox8
             // 
@@ -158,6 +175,8 @@
             this.ClassTextbox.Name = "ClassTextbox";
             this.ClassTextbox.Size = new System.Drawing.Size(135, 20);
             this.ClassTextbox.TabIndex = 9;
+            this.ClassTextbox.Visible = false;
+            this.ClassTextbox.MouseLeave += new System.EventHandler(this.ClassTextbox_MouseLeave);
             // 
             // LevelTextbox
             // 
@@ -165,6 +184,8 @@
             this.LevelTextbox.Name = "LevelTextbox";
             this.LevelTextbox.Size = new System.Drawing.Size(116, 20);
             this.LevelTextbox.TabIndex = 10;
+            this.LevelTextbox.Visible = false;
+            this.LevelTextbox.MouseLeave += new System.EventHandler(this.LevelTextbox_MouseLeave);
             // 
             // AlignmentTextbox
             // 
@@ -172,6 +193,8 @@
             this.AlignmentTextbox.Name = "AlignmentTextbox";
             this.AlignmentTextbox.Size = new System.Drawing.Size(138, 20);
             this.AlignmentTextbox.TabIndex = 11;
+            this.AlignmentTextbox.Visible = false;
+            this.AlignmentTextbox.MouseLeave += new System.EventHandler(this.AlignmentTextbox_MouseLeave);
             // 
             // RaceTextbox
             // 
@@ -179,6 +202,8 @@
             this.RaceTextbox.Name = "RaceTextbox";
             this.RaceTextbox.Size = new System.Drawing.Size(111, 20);
             this.RaceTextbox.TabIndex = 11;
+            this.RaceTextbox.Visible = false;
+            this.RaceTextbox.MouseLeave += new System.EventHandler(this.RaceTextbox_MouseLeave);
             // 
             // ACTextbox
             // 
@@ -259,12 +284,225 @@
             this.CHATextbox.Size = new System.Drawing.Size(48, 20);
             this.CHATextbox.TabIndex = 22;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(140, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "                 ";
+            this.label1.MouseEnter += new System.EventHandler(this.label1_MouseEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(415, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "                 ";
+            this.label2.MouseEnter += new System.EventHandler(this.label2_MouseEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(608, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "                 ";
+            this.label3.MouseEnter += new System.EventHandler(this.label3_MouseEnter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(797, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "                 ";
+            this.label4.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(982, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "                 ";
+            this.label5.MouseEnter += new System.EventHandler(this.label5_MouseEnter);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(568, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 20);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Label";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(797, 198);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 20);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Label";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(1116, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 20);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Label";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label9.Location = new System.Drawing.Point(568, 366);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 20);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Label";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label10.Location = new System.Drawing.Point(1028, 366);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 20);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Label";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label11.Location = new System.Drawing.Point(88, 225);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 20);
+            this.label11.TabIndex = 33;
+            this.label11.Text = "Label";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label12.Location = new System.Drawing.Point(88, 274);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 20);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "Label";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label13.Location = new System.Drawing.Point(88, 320);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(48, 20);
+            this.label13.TabIndex = 35;
+            this.label13.Text = "Label";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label14.Location = new System.Drawing.Point(88, 366);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(48, 20);
+            this.label14.TabIndex = 36;
+            this.label14.Text = "Label";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label15.Location = new System.Drawing.Point(88, 409);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(48, 20);
+            this.label15.TabIndex = 37;
+            this.label15.Text = "Label";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.Transparent;
+            this.label16.Font = new System.Drawing.Font("Calligraphic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label16.Location = new System.Drawing.Point(88, 455);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(48, 20);
+            this.label16.TabIndex = 38;
+            this.label16.Text = "Label";
+            // 
             // CharactersCreatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::D_DHelper.Properties.Resources.CharactersCreatorFon;
             this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.CHATextbox);
             this.Controls.Add(this.WISTextbox);
             this.Controls.Add(this.INTTextbox);
@@ -332,5 +570,21 @@
         public System.Windows.Forms.TextBox INTTextbox;
         public System.Windows.Forms.TextBox WISTextbox;
         public System.Windows.Forms.TextBox CHATextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
     }
 }
--- D-DHelper/D-DHelper/Source/CharactersCreatorForm.cs
+++ D-DHelper/D-DHelper/Source/CharactersCreatorForm.cs
@@ -36,5 +36,68 @@ namespace D_DHelper
         {
             BackToCommonChooseForm(this, prmainform);
         }
+
+        private void InTextToTextboxAndLabel(Label label, TextBox textbox) // For Lables
+        {
+            label.Hide();
+            textbox.Show();
+        }
+
+        private void OutTextToTextboxAndLabel(Label label, TextBox textbox) // For Textbox
+        {
+            textbox.Hide();
+            label.Text = textbox.Text;
+            label.Show();
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            InTextToTextboxAndLabel(label1, CharactersNameTextBox);
+        }
+
+        private void CharactersNameTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            OutTextToTextboxAndLabel(label1, CharactersNameTextBox);
+        }
+
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            InTextToTextboxAndLabel(label2, ClassTextbox);
+        }
+
+        private void ClassTextbox_MouseLeave(object sender, EventArgs e)
+        {
+            OutTextToTextboxAndLabel(label2, ClassTextbox);
+        }
+
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            InTextToTextboxAndLabel(label3, LevelTextbox);
+        }
+
+        private void LevelTextbox_MouseLeave(object sender, EventArgs e)
+        {
+            OutTextToTextboxAndLabel(label3, LevelTextbox);
+        }
+
+        private void label4_MouseEnter(object sender, EventArgs e)
+        {
+            InTextToTextboxAndLabel(label4, RaceTextbox);
+        }
+
+        private void RaceTextbox_MouseLeave(object sender, EventArgs e)
+        {
+            OutTextToTextboxAndLabel(label4, RaceTextbox);
+        }
+
+        private void label5_MouseEnter(object sender, EventArgs e)
+        {
+            InTextToTextboxAndLabel(label5, AlignmentTextbox);
+        }
+
+        private void AlignmentTextbox_MouseLeave(object sender, EventArgs e)
+        {
+            OutTextToTextboxAndLabel(label5, AlignmentTextbox);
+        }
     }
 }
